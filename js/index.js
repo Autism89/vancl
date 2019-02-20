8b5de5b7f51c1d88734840d7dfea50df0c0961b7
liutong <1361622948@qq.com>
2019-02-20 10:05:05 +0800

    这是第一个版本的凡客网站

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+	let s = new Slider({
+		boxDom:$("#box"),
+		btnSize:12,
+		width:1200,
+		height:537,
+		btnColor:"#dddddd",
+		btnHighColor:"#a10000",
+		timeSpace:2000,
+		imgs:["../img/01banner.jpg","../img/02banner.jpg","../img/03banner.jpg","../img/04banner.jpg","../img/05banner.jpg","../img/06banner.jpg"]
+	});
+}
